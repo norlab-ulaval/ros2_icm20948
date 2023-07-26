ab1790cad9bf59e67dee7ca6a443fe23f24d5bbc
BenoitAud <audetb2@gmail.com>
2023-07-26 20:13:14 +0000

    Fixed sensitivity scale factors

--- ros2_icm20948/driver.py
+++ ros2_icm20948/driver.py
@@ -3,7 +3,9 @@ from rclpy.node import Node
 import sensor_msgs.msg
 import geometry_msgs.msg
 import qwiic_icm20948
+import math
 
+from time import sleep
 
 class DriverNode(Node):
     def __init__(self):
@@ -28,9 +30,12 @@ def main(args=None):
     imu = qwiic_icm20948.QwiicIcm20948()
     while not imu.connected and rclpy.ok():
         node.log_info("The Qwiic ICM20948 device isn't connected to the system. Please check your connection")
-
+    
     imu.begin()
 
+    imu.setFullScaleRangeGyro(qwiic_icm20948.dps2000)
+    imu.setFullScaleRangeAccel(qwiic_icm20948.gpm16)
+    
     imu_msg = sensor_msgs.msg.Imu()
     mag_msg = sensor_msgs.msg.MagneticField()
     rate = node.create_rate(100)
@@ -39,21 +44,22 @@ def main(args=None):
             imu.getAgmt()
             imu_msg.header.stamp = node.get_clock().now().to_msg()
             imu_msg.header.frame_id = "imu"
-            imu_msg.linear_acceleration.x = imu.axRaw
-            imu_msg.linear_acceleration.y = imu.ayRaw
-            imu_msg.linear_acceleration.z = imu.azRaw
-            imu_msg.angular_velocity.x = imu.gxRaw
-            imu_msg.angular_velocity.y = imu.gyRaw
-            imu_msg.angular_velocity.z = imu.gzRaw
+            imu_msg.linear_acceleration.x = imu.axRaw * 9.81 / 2048.0
+            imu_msg.linear_acceleration.y = imu.ayRaw * 9.81 / 2048.0
+            imu_msg.linear_acceleration.z = imu.azRaw * 9.81 / 2048.0
+            imu_msg.angular_velocity.x = imu.gxRaw * math.pi / (16.4 * 180)
+            imu_msg.angular_velocity.y = imu.gyRaw * math.pi / (16.4 * 180)
+            imu_msg.angular_velocity.z = imu.gzRaw * math.pi / (16.4 * 180)
             imu_msg.orientation_covariance[0] = -1
             node.publish_imu(imu_msg)
 
             mag_msg.header.stamp = imu_msg.header.stamp
             mag_msg.header.frame_id = "imu"
-            mag_msg.magnetic_field.x = imu.mxRaw
-            mag_msg.magnetic_field.y = imu.myRaw
-            mag_msg.magnetic_field.z = imu.mzRaw
+            mag_msg.magnetic_field.x = imu.mxRaw * 1e-6 / 0.15
+            mag_msg.magnetic_field.y = imu.myRaw * 1e-6 / 0.15
+            mag_msg.magnetic_field.z = imu.mzRaw * 1e-6 / 0.15
             node.publish_mag(mag_msg)
+
         rclpy.spin_once(node)
         rate.sleep()
     node.destroy_node()
