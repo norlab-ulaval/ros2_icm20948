cfa7ab4529efec3edb9c181a381ae73160da159c
Damien LaRocque <phicoltan@gmail.com>
2023-10-16 14:54:23 -0400

    Refactor node; put everything in the class

--- ros2_icm20948/icm20948_node.py
+++ ros2_icm20948/icm20948_node.py
@@ -1,75 +1,82 @@
 import math
-from time import sleep
 
-import geometry_msgs.msg
 import qwiic_icm20948
 import rclpy
 import sensor_msgs.msg
 from rclpy.node import Node
 
 
-class DriverNode(Node):
+class ICM20948Node(Node):
     def __init__(self):
-        super().__init__("driver_node")
-        self.imu_publisher = self.create_publisher(
-            sensor_msgs.msg.Imu, "/imu/data_raw", 10
-        )
-        self.mag_publisher = self.create_publisher(
+        super().__init__("icm20948_node")
+
+        # Logger
+        self.logger = self.get_logger()
+
+        # Parameters
+        self.declare_parameter("i2c_address", 0x69)
+        i2c_addr = self.get_parameter("i2c_address").get_parameter_value().integer_value
+        self.i2c_addr = i2c_addr
+
+        self.declare_parameter("frame_id", "imu_icm20948")
+        frame_id = self.get_parameter("frame_id").get_parameter_value().string_value
+        self.frame_id = frame_id
+
+        self.declare_parameter("pub_rate", 100)
+        pub_rate = self.get_parameter("pub_rate").get_parameter_value().integer_value
+        self.pub_rate = pub_rate
+
+        # IMU instance
+        self.imu = qwiic_icm20948.QwiicIcm20948(address=self.i2c_addr)
+        if not self.imu.connected:
+            self.logger.info(
+                "The Qwiic ICM20948 device isn't connected to the system. Please check your connection."
+            )
+        self.imu.begin()
+        self.imu.setFullScaleRangeGyro(qwiic_icm20948.dps2000)
+        self.imu.setFullScaleRangeAccel(qwiic_icm20948.gpm16)
+
+        # Publishers
+        self.imu_pub_ = self.create_publisher(sensor_msgs.msg.Imu, "/imu/data_raw", 10)
+        self.pub_clk_ = self.create_timer(1 / self.imu_rate, self.publish_cback)
+
+        self.mag_pub_ = self.create_publisher(
             sensor_msgs.msg.MagneticField, "/imu/mag_raw", 10
         )
+        self.mag_clk_ = self.create_timer(1 / self.mag_rate, self.publish_mag_cback)
 
-    def log_info(self, message):
-        self.get_logger().info(message)
+    def publish_cback(self):
+        imu_msg = sensor_msgs.msg.Imu()
+        mag_msg = sensor_msgs.msg.MagneticField()
+        if self.imu.dataReady():
+            self.imu.getAgmt()
 
-    def publish_imu(self, msg):
-        self.imu_publisher.publish(msg)
+            imu_msg.header.stamp = self.get_clock().now().to_msg()
+            imu_msg.header.frame_id = "icm20948"
+            imu_msg.linear_acceleration.x = self.imu.axRaw * 9.81 / 2048.0
+            imu_msg.linear_acceleration.y = self.imu.ayRaw * 9.81 / 2048.0
+            imu_msg.linear_acceleration.z = self.imu.azRaw * 9.81 / 2048.0
+            imu_msg.angular_velocity.x = self.imu.gxRaw * math.pi / (16.4 * 180)
+            imu_msg.angular_velocity.y = self.imu.gyRaw * math.pi / (16.4 * 180)
+            imu_msg.angular_velocity.z = self.imu.gzRaw * math.pi / (16.4 * 180)
+            imu_msg.orientation_covariance[0] = -1
 
-    def publish_mag(self, msg):
-        self.mag_publisher.publish(msg)
+            mag_msg.header.stamp = imu_msg.header.stamp
+            mag_msg.header.frame_id = "imu"
+            mag_msg.magnetic_field.x = self.imu.mxRaw * 1e-6 / 0.15
+            mag_msg.magnetic_field.y = self.imu.myRaw * 1e-6 / 0.15
+            mag_msg.magnetic_field.z = self.imu.mzRaw * 1e-6 / 0.15
+
+        self.imu_pub_.publish(imu_msg)
+        self.mag_pub_.publish(mag_msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = DriverNode()
+    icm20948_node = ICM20948Node()
+    rclpy.spin(icm20948_node)
 
-    imu = qwiic_icm20948.QwiicIcm20948()
-    while not imu.connected and rclpy.ok():
-        node.log_info(
-            "The Qwiic ICM20948 device isn't connected to the system. Please check your connection"
-        )
-
-    imu.begin()
-
-    imu.setFullScaleRangeGyro(qwiic_icm20948.dps2000)
-    imu.setFullScaleRangeAccel(qwiic_icm20948.gpm16)
-
-    imu_msg = sensor_msgs.msg.Imu()
-    mag_msg = sensor_msgs.msg.MagneticField()
-    rate = node.create_rate(100)
-    while rclpy.ok():
-        if imu.dataReady():
-            imu.getAgmt()
-            imu_msg.header.stamp = node.get_clock().now().to_msg()
-            imu_msg.header.frame_id = "icm20948"
-            imu_msg.linear_acceleration.x = imu.axRaw * 9.81 / 2048.0
-            imu_msg.linear_acceleration.y = imu.ayRaw * 9.81 / 2048.0
-            imu_msg.linear_acceleration.z = imu.azRaw * 9.81 / 2048.0
-            imu_msg.angular_velocity.x = imu.gxRaw * math.pi / (16.4 * 180)
-            imu_msg.angular_velocity.y = imu.gyRaw * math.pi / (16.4 * 180)
-            imu_msg.angular_velocity.z = imu.gzRaw * math.pi / (16.4 * 180)
-            imu_msg.orientation_covariance[0] = -1
-            node.publish_imu(imu_msg)
-
-            mag_msg.header.stamp = imu_msg.header.stamp
-            mag_msg.header.frame_id = "imu"
-            mag_msg.magnetic_field.x = imu.mxRaw * 1e-6 / 0.15
-            mag_msg.magnetic_field.y = imu.myRaw * 1e-6 / 0.15
-            mag_msg.magnetic_field.z = imu.mzRaw * 1e-6 / 0.15
-            node.publish_mag(mag_msg)
-
-        rclpy.spin_once(node)
-        rate.sleep()
-    node.destroy_node()
+    icm20948_node.destroy_node()
     rclpy.shutdown()
 
 
--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup
 
-package_name = 'ros2_icm20948'
+package_name = "ros2_icm20948"
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version="0.0.1",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        "setuptools",
+        "sparkfun-qwiic-icm20948",
+    ],
     zip_safe=True,
-    maintainer='Simon-Pierre Deschênes',
-    maintainer_email='simon-pierre.deschenes.1@ulaval.ca',
-    description='Driver for the ICM-20948 IMU',
-    license='BSD-2.0',
+    maintainer="Simon-Pierre Deschênes",
+    maintainer_email="simon-pierre.deschenes.1@ulaval.ca",
+    description="Driver for the ICM-20948 IMU",
+    license="BSD-2.0",
     entry_points={
-        'console_scripts': [
-            'driver = ros2_icm20948.driver:main',
+        "console_scripts": [
+            "icm20948_node = ros2_icm20948.icm20948_node:main",
         ],
     },
 )
